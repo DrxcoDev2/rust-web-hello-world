6aa3aae8ac3aadb18642be383d9890037ccb6cee
DrxcoDev2 <drxcofn2023@gmail.com>
2025-07-23 19:57:52 +0200

    Add a rust template creator

---
+++ rust-template.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Copyright (c) 2025 DrxcoDev2
+# MIT License
+# Use this file to clean the template 
+# and create a new project
+
+error() {
+    echo "Error: $1"
+    exit 1
+}
+
+echo "Clonando el repositorio..."
+git clone https://github.com/DrxcoDev2/rust-web-hello-world.git
+cd rust-web-hello-world
+
+echo "Eliminando archivos no necesarios..."
+rm -rf .git
+rm  templates/index.hbs
+rm README.md
+rm Makefile
+rm LICENSE
+
+echo "Limpiando el proyecto..."
+cargo clean
+
+echo "Forzando la eliminacion de /target"
+if [ -d "target" ]; then
+    rm -rf target
+    echo "Target eliminado correctamente"
+fi
+
+input=""
+while [ -z "$input" ]; do
+    read -p "Ingrese el nombre del proyecto: " input
+    if [ -z "$input" ]; then
+        error "El nombre del proyecto no puede estar vacio."
+    fi
+    cd ..
+    mv rust-web-hello-world $input
+    echo "Proyecto renombrado correctamente a $input"
+    cd $input
+done
+
+
+
+
