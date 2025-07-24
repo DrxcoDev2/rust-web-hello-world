f98f39f2fb2eab9399f88718494f529a8e65d032
DrxcoDev2 <drxcofn2023@gmail.com>
2025-07-24 13:54:56 +0200

    workflows

--- rust-template.sh
+++ rust-template.sh
@@ -5,8 +5,17 @@
 # Use this file to clean the template 
 # and create a new project
 
+# COLORS
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+
 error() {
-    echo "Error: $1"
+    echo "${RED}Error: $1${NC}"
     exit 1
 }
 
@@ -20,6 +29,7 @@ rm  templates/index.hbs
 rm README.md
 rm Makefile
 rm LICENSE
+rm -rf .github/
 
 echo "Limpiando el proyecto..."
 cargo clean
@@ -49,8 +59,46 @@ while [ -z "$input2" ]; do
         error "El nombre del archivo de entorno no puede estar vacio."
     fi
     touch .env
-    echo "Archivo de entorno creado correctamente"
+    echo "${GREEN}Archivo de entorno creado correctamente${NC}"
 done
+
+input3=""
+while [ -z "$input3" ]; do
+    read -p "Te gustaria autoelminar este archivo? (y/n):   " input3
+    if [ "$input3" = "y" ]; then
+        rm rust-template.sh
+        echo "${GREEN}Archivo rust-template.sh eliminado correctamente${NC}"
+    fi
+    if [ "$input3" = "n" ]; then
+        echo "${YELLOW}Archivo rust-template.sh no eliminado${NC}"
+    fi
+
+done
+
+input4=""
+while [ -z "$input4" ]; do
+    read -p "Te gustaria inicializar git? (y/n):   " input4
+    if [ "$input4" = "y" ]; then
+        git init
+        echo "${GREEN}Git inicializado correctamente${NC}"
+    fi
+    if [ "$input4" = "n" ]; then
+        echo "${YELLOW}Git no inicializado${NC}"
+    fi
+
+    input4a=""
+    while [ -z "$input4a" ]; do
+        read -p "Ingrese el repositorio remoto: " input4a
+        if [ -z "$input4a" ]; then
+            error "El repositorio remoto no puede estar vacio."
+        fi
+        git remote add origin $input4a
+        echo "${GREEN}Repositorio remoto agregado correctamente${NC}"
+    done
+
+done
+
+    
 
 
 
